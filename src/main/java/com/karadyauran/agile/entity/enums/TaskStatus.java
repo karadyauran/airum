23d774532905db7388cedcb55c6ddb05ea6aa80d
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2023-12-18 10:56:36 +0100

    Added new task statuses

--- src/main/java/com/karadyauran/agile/entity/enums/TaskStatus.java
+++ src/main/java/com/karadyauran/agile/entity/enums/TaskStatus.java
@@ -3,5 +3,17 @@ package com.karadyauran.agile.entity.enums;
 public enum TaskStatus {
   TODO,
   IN_PROGRESS,
-  DONE
+  DONE,
+  CANCELLED,
+  ON_HOLD,
+  REVIEW,
+  APPROVED,
+  REJECTED,
+  CLOSED,
+  REOPENED,
+  RESOLVED,
+  VERIFIED,
+  INCOMPLETE,
+  EXPIRED,
+  DELETED
 }

26aa87921ec8a2679e4c95648512f57d637d06e1
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-01-10 10:40:01 +0100

    Reverted changes in task repository

--- src/main/java/com/karadyauran/agile/repository/TaskRepository.java
+++ src/main/java/com/karadyauran/agile/repository/TaskRepository.java
@@ -2,8 +2,6 @@ package com.karadyauran.agile.repository;
 
 import com.karadyauran.agile.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -11,6 +9,5 @@ import java.util.UUID;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID>
 {
-    @Query("SELECT t FROM Task t WHERE t.taskId = CAST(:taskId AS CHAR)")
-    Task findByTaskId(@Param("taskId") UUID taskId);
+    Task findByTaskId(UUID taskId);
 }

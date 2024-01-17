25ed3b41b60841dc70a11ad7a850817e776f3420
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-01-17 16:42:04 +0100

    Added Error messages and IdNotFountException

---
+++ src/main/java/com/karadyauran/agile/exception/IdNotFountException.java
@@ -0,0 +1,9 @@
+package com.karadyauran.agile.exception;
+
+public class IdNotFountException extends Exception
+{
+    public IdNotFountException (String message)
+    {
+        super(message);
+    }
+}
---
+++ src/main/java/com/karadyauran/agile/exception/message/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.karadyauran.agile.exception.message;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ErrorMessage
+{
+    public static final String USER_DOESNT_EXIST = "User doesn't exist";
+    public static final String USERNAME_ALREADY_EXISTS = "Username already exists";
+    public static final String PROJECT_DOESNT_EXIST = "Project doesn't exist";
+    public static final String TASK_NOT_FOUND = "Task not found";
+    public static final String ID_NOT_FOUND = "ID not found";
+}

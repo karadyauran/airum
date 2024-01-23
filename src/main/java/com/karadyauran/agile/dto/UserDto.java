5b65b33a42c6e2cf468b1bb8da9c29a2a9b1fda3
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-01-23 13:46:39 +0100

    Added some parameters for UserDto

--- src/main/java/com/karadyauran/agile/dto/UserDto.java
+++ src/main/java/com/karadyauran/agile/dto/UserDto.java
@@ -1,12 +1,15 @@
 package com.karadyauran.agile.dto;
 
 import com.karadyauran.agile.entity.Project;
+import com.karadyauran.agile.entity.Task;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Builder;
+import lombok.Value;
 
 import java.util.List;
 
+@Value
 @Builder
 @Tag(name = "User", description = "Dto for user")
 public class UserDto
@@ -17,6 +20,8 @@ public class UserDto
     String name;
     @Schema(name = "User surname", description = "Surname for User")
     String surname;
-    @Schema(name = "Owned Projects", description = "Project user owned")
-    List<Project> ownedProjects;
+    @Schema(name = "List of projects", description = "Project user has")
+    List<Project> projects;
+    @Schema(name = "List of tasks", description = "Tasks user has")
+    List<Task> assignedTasks;
 }

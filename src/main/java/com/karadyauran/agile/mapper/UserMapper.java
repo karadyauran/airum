65b1717dc1e2eb96cfda2198795d8ed59b2f39c4
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-01-30 23:11:51 +0100

    Added mapping for user

--- src/main/java/com/karadyauran/agile/mapper/UserMapper.java
+++ src/main/java/com/karadyauran/agile/mapper/UserMapper.java
@@ -3,15 +3,22 @@ package com.karadyauran.agile.mapper;
 import com.karadyauran.agile.dto.UserDto;
 import com.karadyauran.agile.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {
+        Timestamp.class,
+        LocalDateTime.class
+})
 public interface UserMapper
 {
     UserDto toDto(User user);
 
     List<UserDto> toDtoList(List<User> users);
 
+    @Mapping(target = "registeredAt", expression = "java(Timestamp.valueOf(LocalDateTime.now()))")
     User toEntity(UserDto userDto);
 }

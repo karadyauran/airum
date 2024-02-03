62f4835b3007437ff4e107ea5570e6796aa91e15
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-02-03 11:58:09 +0100

    Created attachment entity

---
+++ src/main/java/com/karadyauran/agile/entity/Attachment.java
@@ -0,0 +1,64 @@
+package com.karadyauran.agile.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "attachments")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Attachment
+{
+    @Id
+    @UuidGenerator
+    @Column(name = "id")
+    UUID id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "path")
+    String path;
+
+    @Column(name = "task_id")
+    UUID taskId;
+
+    @Column(name = "user_id")
+    UUID userId;
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Attachment that = (Attachment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format(
+                "%s -> %s",
+                this.name,
+                this.path
+        );
+    }
+}

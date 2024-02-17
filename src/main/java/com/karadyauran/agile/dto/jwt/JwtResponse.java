729625b4aabc3d5f338865ce505bd40b8597a163
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-02-17 10:22:32 +0100

    Add Auth Controller

---
+++ src/main/java/com/karadyauran/agile/dto/jwt/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.karadyauran.agile.dto.jwt;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Schema(description = "Here you get token to authorize")
+public class JwtResponse {
+
+    private final String token;
+}

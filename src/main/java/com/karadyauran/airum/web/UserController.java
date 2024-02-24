5c6f8aaa6002c431db116f56268d33cc24e77b4c
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-02-24 10:13:37 +0100

    created documentation for user controller

--- src/main/java/com/karadyauran/airum/web/UserController.java
+++ src/main/java/com/karadyauran/airum/web/UserController.java
@@ -3,6 +3,10 @@ package com.karadyauran.airum.web;
 import com.karadyauran.airum.api.UserApi;
 import com.karadyauran.airum.dto.UserDto;
 import com.karadyauran.airum.service.interf.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -24,6 +28,23 @@ public class UserController implements UserApi
 {
     UserService service;
 
+    @Operation(summary = "Gets user by it's id",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> findById(UUID id)
     {
@@ -32,6 +53,23 @@ public class UserController implements UserApi
                 .body(service.getUserById(id));
     }
 
+    @Operation(summary = "Gets user by it's username",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this fields for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> findByIUsername(String username)
     {
@@ -40,6 +78,23 @@ public class UserController implements UserApi
                 .body(service.getUserByUsername(username));
     }
 
+    @Operation(summary = "Finds all users in database",
+            description = "Returns List of UserDto entities",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<List<UserDto>> findAll()
     {
@@ -48,6 +103,23 @@ public class UserController implements UserApi
                 .body(service.getAll());
     }
 
+    @Operation(summary = "Changes username for User",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> changeUsername(UUID id, String username)
     {
@@ -56,6 +128,23 @@ public class UserController implements UserApi
                 .body(service.changeUsername(id, username));
     }
 
+    @Operation(summary = "Changes firstname for User",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> changeFirstname(UUID id, String firstname)
     {
@@ -64,6 +153,23 @@ public class UserController implements UserApi
                 .body(service.changeFirstname(id, firstname));
     }
 
+    @Operation(summary = "Changes surname for User",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> changeSurname(UUID id, String surname)
     {
@@ -72,6 +178,23 @@ public class UserController implements UserApi
                 .body(service.changeSurname(id, surname));
     }
 
+    @Operation(summary = "Changes email for User",
+            description = "Returns UserDto entity",
+            tags = "USERS",
+            // TODO make documentation on github
+            /*externalDocs = @ExternalDocumentation(
+                    description = "Вот тут вся документация",
+                    url = "https://google.com/"
+            ),*/
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "found"),
+                    @ApiResponse(responseCode = "500", description = "have not been fount")
+            }
+            // TODO fill this field for security requirements
+            /*security = {
+                    @SecurityRequirement(name = "требования к безопасности")
+            },*/
+    )
     @Override
     public ResponseEntity<UserDto> changeEmail(UUID id, String email)
     {
@@ -80,6 +203,11 @@ public class UserController implements UserApi
                 .body(service.changeEmail(id, email));
     }
 
+    // TODO translate on english
+    /**
+     * Этот каскадный тип применяет операцию remove()
+     * всем связанным сущностям, когда родительская сущность удаляется.
+     */
     @Override
     public ResponseEntity<Void> deleteById(UUID id)
     {
@@ -87,6 +215,21 @@ public class UserController implements UserApi
         return ResponseEntity.ok().build();
     }
 
+
+    // TODO translate on english
+    @Operation(summary = "Creates new user",
+            description = "UserDto would be the answer",
+            tags = "USERS",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "USERS DATA",
+                    required = true, //обязательно ли тело запроса для сохранения
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UserDto.class) // points on class,
+                            // который служит примером реализации схемы для объекта API
+                    )
+            )
+    )
     @Override
     public ResponseEntity<UserDto> create(UserDto dto)
     {
